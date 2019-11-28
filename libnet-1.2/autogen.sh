f91aaff346ba76205816716f8f65100c588c608c
Doug <doug.leith@gmail.com>
2019-11-28 08:02:05 +0000

    file tidy up

--- libnet-1.2/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# A bootstrapping script that can be used to generate the autoconf,
-# automake and libtool-related scripts of the build process.  Only
-# useful for developers updating Makefile.am, configure.ac etc.
-# Regular users building from released tarballs can use the shipped
-# configure script, that generates Makefile's from Makefile.in's
-
-autoreconf -W portability -vif $*
